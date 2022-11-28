9de0a9aab095e8de7fa1f2ea831ec0e2f3e00310
Aerek Szulc <e.szulc@gmail.com>
2022-11-27 20:03:09 -0500

    More functions and example
    1. updated mechanism to include getgroups, breakoutgroups, addtogroup, and group scores
    2. example for voting that needs cleanup

--- js/diversity-mechanism.js
+++ js/diversity-mechanism.js
@@ -1,6 +1,141 @@
 class DiversityMechanism {
 
-    static getNormalizedScore( entities, attributes ){
+    constructor( attributes ) {
+        this.attributes = attributes;
+    }
+
+    // break a larger group into equal smaller groups
+    breakoutGroups( group, count ){
+        
+        var breakoutGroups = [];
+        var numberOfGroups = Math.ceil( group.length / count );
+        var totals = [];
+
+        for( var i=0; i<numberOfGroups; i++ ){
+            breakoutGroups.push([]);
+        }
+
+        group.forEach(
+            (g,g_i) => {
+
+                var highestScore = 0;
+                var currentCandidateGroupIndex = 0;
+                
+                breakoutGroups.forEach(
+                    (bg,bg_i) => {
+
+                        var newGroup = [...bg];
+                        newGroup.push(g);
+                        var s = this.getNormalizedScores( newGroup );
+                        var userscore = s.entities[s.entities.length - 1].score.normalized;
+                        
+                        if( 
+                            userscore > highestScore 
+                            &&  bg.length < count
+                        ){
+
+                            highestScore = userscore;
+                            currentCandidateGroupIndex = bg_i;
+                        }
+                    }
+                )
+
+                breakoutGroups[currentCandidateGroupIndex].push( g );
+            }
+        );
+
+        breakoutGroups.forEach(
+            (bg,bg_i) => {
+                totals.push( this.getNormalizedScores( bg ) )
+            }
+        );
+
+
+        return totals;
+    }
+
+    // add a certain number from proposed group to base group
+    addToGroup( group, candidateGroup, count ){
+
+        var newGroup = [...group];
+
+        for( var i=0; i<count; i++ ){
+
+            var highestScore = 0;
+            var currentCandidate = {};
+            var currentCandidateIndex = 0;
+
+            candidateGroup.forEach(
+                (g,i) => {
+
+                    var potentialGroup = [...group];
+                    potentialGroup.push( g );
+
+                    var s = this.getNormalizedScores( potentialGroup );
+
+                    if( s.score > highestScore ){
+
+                        highestScore = s.score;
+                        currentCandidate = g;
+                        currentCandidateIndex = i;
+                    }
+                }
+            )
+            newGroup.push( currentCandidate );
+            
+            candidateGroup.splice(currentCandidateIndex, 1);
+        }
+
+        return this.getNormalizedScores( newGroup );
+    }
+
+    // pull a certain number from group 
+    getGroup( group, count ){
+
+        var originalGroup = [...group];
+        var newGroup = [];
+
+        for( var i=0; i<count; i++ ){
+
+            var highestScore = 0;
+            var currentCandidate = {};
+            var currentCandidateIndex = 0;
+
+            originalGroup.forEach(
+                (g,i) => {
+
+                    var potentialGroup = [...newGroup];
+                    potentialGroup.push( g );
+                    var s = this.getNormalizedScores( potentialGroup );
+                    if( s.score > highestScore ){
+
+                        highestScore = s.score;
+                        currentCandidate = g;
+                        currentCandidateIndex = i;
+                    }
+                }
+            )
+
+            newGroup.push( currentCandidate );
+            
+            originalGroup.splice( currentCandidateIndex, 1 );
+        }
+
+        return this.getNormalizedScores( newGroup );
+    }
+
+    getGroupsNormalizedScores( groups ){
+        var totals = {};
+
+        for( var g in groups ){
+            totals[g] = {};
+            totals[g] = this.getNormalizedScores( groups[g] );
+        }
+
+        return totals;
+    }
+
+    getNormalizedScores( entities ){
         var avm = new Object();
         var count = entities.length;
         var totals = {};
@@ -9,7 +144,7 @@ class DiversityMechanism {
             (o_i) => {
                 o_i.score = {};
                 o_i.score.normalized = 0;
-                attributes.forEach( 
+                this.attributes.forEach( 
                     (a_i) => {
                         if( !avm.hasOwnProperty(a_i) ){
                             avm[a_i] = {};
@@ -20,12 +155,18 @@ class DiversityMechanism {
                         avm[a_i][o_i[a_i]]++;
                         var n = Math.log( Number( avm[a_i][o_i[a_i]] ) );
                         var d = Math.log( Number( count ) );
-                        var s = 1 - (n / d);
+                        var s;
+                        if( count == 1 ){
+                            s = 1;
+                        }else{
+                            s = 1 - (n / d);
+                        }
+                        
                         o_i.score[a_i] = s;
                         o_i.score.normalized += s;
                     }
                 )
-                o_i.score.normalized = (o_i.score.normalized / attributes.length);
+                o_i.score.normalized = (o_i.score.normalized / this.attributes.length);
                 totals.score += o_i.score.normalized;
             }
         );
